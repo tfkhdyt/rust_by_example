9f9c2d3fbaad94728496aa71803fbe143e150a1a
tfkhdyt <me@tfkhdyt.my.id>
2023-10-03 06:45:14 +0700

    feat(hello_world): learn more about formatting

--- 1_hello_world/src/main.rs
+++ 1_hello_world/src/main.rs
@@ -1,37 +1,142 @@
-use std::fmt;
+use std::fmt::{self, Display};
 
-#[derive(Debug)]
-struct MinMax(i64, i64);
+// #[derive(Debug)]
+// struct MinMax(i64, i64);
 
-impl fmt::Display for MinMax {
+// impl fmt::Display for MinMax {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "({}, {})", self.0, self.1)
+//     }
+// }
+
+// #[derive(Debug)]
+// struct Point2D {
+//     x: f64,
+//     y: f64,
+// }
+// impl fmt::Display for Point2D {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "x: {}, y: {}", self.x, self.y)
+//     }
+// }
+
+// #[derive(Debug)]
+// struct Complex {
+//     real: f64,
+//     imag: f64,
+// }
+// impl fmt::Display for Complex {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "{} + {}", self.real, self.imag)
+//     }
+// }
+
+// struct Structure(i32);
+// impl fmt::Display for Structure {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "{}", self.0)
+//     }
+// }
+
+// struct List(Vec<i32>);
+
+// impl fmt::Display for List {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         let vec = &self.0;
+//         write!(f, "[")?;
+//         for (count, v) in vec.iter().enumerate() {
+//             if count != 0 {
+//                 write!(f, ", ")?;
+//             }
+//             write!(f, "{}: {}", count, v)?;
+//         }
+//         write!(f, "]")
+//     }
+// }
+
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl Display for City {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+        write!(
+            f,
+            "{}: {:.3}°{} {:.3}°{}",
+            self.name,
+            self.lat.abs(),
+            lat_c,
+            self.lon.abs(),
+            lon_c
+        )
     }
 }
 
 #[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "x: {}, y: {}", self.x, self.y)
-    }
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
 }
 
-#[derive(Debug)]
-struct Complex {
-    real: f64,
-    imag: f64,
-}
-impl fmt::Display for Complex {
+impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} + {}", self.real, self.imag)
+        write!(
+            f,
+            "0x{:0>2X}{:0>2X}{:0>2X}",
+            self.red, self.green, self.blue
+        )
     }
 }
 
 fn main() {
+    // for city in [
+    //     City {
+    //         name: "Dublin",
+    //         lat: 53.347776,
+    //         lon: -6.259722,
+    //     },
+    //     City {
+    //         name: "Oslo",
+    //         lat: 59.95,
+    //         lon: 10.75,
+    //     },
+    //     City {
+    //         name: "Vancouver",
+    //         lat: 49.25,
+    //         lon: -123.1,
+    //     },
+    // ] {
+    //     println!("{}", city);
+    // }
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ] {
+        // println!("{:?}", color);
+        println!("{}", color);
+    }
+
+    // let v = List(vec![1, 2, 3]);
+    // println!("{}", v);
+
     // println!("Hello, world!");
     // println!("I'm a Rustacean!");
 
@@ -114,11 +219,11 @@ fn main() {
     // println!("Display: {}", point);
     // println!("Debug: {:?}", point)
 
-    let complex = Complex {
-        real: 3.3,
-        imag: 7.2,
-    };
+    // let complex = Complex {
+    //     real: 3.3,
+    //     imag: 7.2,
+    // };
 
-    println!("Display: {}", complex);
-    println!("Debug: {:?}", complex);
+    // println!("Display: {}", complex);
+    // println!("Debug: {:?}", complex);
 }
